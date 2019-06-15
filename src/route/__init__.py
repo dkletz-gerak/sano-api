a73673beacf47df0d994c63bef73f3f59a9d9eaf
Ubuntu <13516037@std.stei.itb.ac.id>
2019-06-15 16:02:32 +0000

    Add category to main route

--- src/route/__init__.py
+++ src/route/__init__.py
@@ -1,7 +1,9 @@
 from core.router import Router
 from src.route.user import user_router
+from src.route.category import category_router
 
 group_router = Router()
 group_router.group("/user", user_router)
+group_router.group("/category", category_router)
 
 __all__ = [group_router]
