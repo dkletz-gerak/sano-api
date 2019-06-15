e95195f643b41fbc36b812075158a5d639e81aa1
Gery Wahyu <gerywahyunugraha@gmail.com>
2019-06-16 02:03:35 +0700

    Fix error

--- src/route/location.py
+++ src/route/location.py
@@ -2,4 +2,4 @@ from core.router import Router
 from src.controller.location import *
 
 location_router = Router()
-location_router.get("/<int:event_id>", get_location_by_id) 
+location_router.get("/<int:location_id>", get_location_by_id) 
